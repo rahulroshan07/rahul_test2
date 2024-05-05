07c5575ae6c47a1afa6695c8ee63828944c5ea38
rahulroshan07 <roshanrahul121@gmail.com>
2024-05-05 18:07:26 +0530

    hello

--- git.py
+++ git.py
@@ -2,8 +2,7 @@ print("Rahul the teacher, happy techer's day.")
 print('pushkar')
 print('anand')
 print("pooooo")
-<<<<<<< HEAD
-print ("hello")
-=======
+
+
 print('paaji bole pom pom pom pom')
->>>>>>> cae6c4025016ea08a52c295c765064ba24164a4f
+
